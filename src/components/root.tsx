5c376a63d5ab62afdcef69a4a8d1288ac700004e
Ari Zeto <arizetocs@gmail.com>
2024-04-06 16:12:30 -0700

    Footer

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,9 +1,26 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const AlignFooter = styled.div`
+  margin-top: auto;
+`;
 export default function Root() {
   return (
     <>
-      <Navbar />
-      <h1>Ignore this layout. Just setting basic things up.</h1>
+      <Container>
+        <Navbar />
+        <h1>Ignore this layout. Just setting basic things up.</h1>
+        <AlignFooter>
+          <Footer />
+        </AlignFooter>
+      </Container>
     </>
   );
 }
