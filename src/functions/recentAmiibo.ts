71ebec71411dcbccab277b39de8f730adf7616f8
Ari Zeto <arizetocs@gmail.com>
2024-04-17 19:37:51 -0700

    Added proper citation for inspiration howto/kickstart calculate date handling for recent releases for amiibo

--- src/functions/recentAmiibo.ts
+++ src/functions/recentAmiibo.ts
@@ -4,6 +4,14 @@
 // This function creates a new Date, calculates the date from a year ago today, grabs the day, month, handles conversion.
 // If the date is >= the release date of the amiibo, it is a truthy value, otherwise false.
 // Used to ensure 'recent released' amiibos are allocated properly on the homepage.
+
+// Sources on how to implement:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDate
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getFullYear
+
+// Utilized library information from MDN and inspired by this stackoverflow post:
+// https://stackoverflow.com/questions/11649672/find-out-the-previous-year-date-from-current-date-in-javascript
 export default function calculateDateRecentAmiibo(amiiboDate) {
     return amiiboDate
         ? new Date(amiiboDate) >=
