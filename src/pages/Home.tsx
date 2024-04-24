57449c876bb430ffb45b6c85a16581f6e77dae04
Ari Zeto <arizetocs@gmail.com>
2024-04-24 12:33:50 -0700

    WIP

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ import GetAmiibo from '../requests/fetchAmiiboList';
 import AmiiboCard from '../components/AmiiboCard';
 import React from 'react';
 
-// Deconstruct data from TanStack fucntion 'GetAmiibo', utilize its state management for checking for data (isLoading, data, error), render conditionally.
-// Calls function to filter out recent releases, sends to componetized card function component (reused assets for showcasing amiibo).
+// Deconstruct data from TanStack function 'GetAmiibo', utilize its state management for checking for data (isLoading, data, error), render conditionally.
+// Calls function to filter out recent releases, sends to component card function component (reused assets for showcasing amiibo).
 export default function Home() {
     const { data, isLoading, error } = GetAmiibo();
 
