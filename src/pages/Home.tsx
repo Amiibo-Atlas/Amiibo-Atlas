5529cb48068df916caf64326d78855b3c2c69f80
Ari Zeto <arizetocs@gmail.com>
2024-04-25 09:29:19 -0700

    Homepage can now be filtered between recent and show all. state maintained by redux. pagination should be implemented but that can occur in a later progress report

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,24 +2,37 @@ import filterRecentReleases from '../requests/filterRecentReleases';
 import GetAmiibo from '../requests/fetchAmiiboList';
 import AmiiboCard from '../components/AmiiboCard';
 import myImage from '../assets/amiibo.png';
+import { useState } from 'react';
+import { useAppSelector } from '../redux/hooks';
 
 // Deconstruct data from TanStack function 'GetAmiibo', utilize its state management for checking for data (isLoading, data, error), render conditionally.
 // Calls function to filter out recent releases, sends to component card function component (reused assets for showcasing amiibo).
 export default function Home() {
-    const { data, isLoading, error } = GetAmiibo();
+    const [allAmiibo, setAllAmiibo] = useState(false);
+    const { isLoading, error } = GetAmiibo();
+
+    // Selecting amiibo data from the redux store...
+    const amiiboDataRedux = useAppSelector((state) => state.allAmiiboSlice.amiibos);
+    console.log('Testing...: ', amiiboDataRedux);
+
+    // Filter amiibo data based on allAmiibo state
+    const filterAmiiboAllOrRecent = allAmiibo
+        ? amiiboDataRedux
+        : filterRecentReleases(amiiboDataRedux);
+
+    console.log('Recent...: ', filterAmiiboAllOrRecent);
 
     if (isLoading) return <p>Loading...</p>;
     if (error) return <p>{error.message}</p>;
-
-    const recentRelease = filterRecentReleases(data);
-    console.log(recentRelease);
 
     return (
         <>
             <h1>Amiibo Atlas</h1>
             <img src={myImage} />
-            <h2> New Releases | See all</h2>
-            <AmiiboCard />
+            <h2>
+                New Releases | <button onClick={() => setAllAmiibo(!allAmiibo)}>Show all?</button>
+            </h2>
+            <AmiiboCard amiiboProps={filterAmiiboAllOrRecent} />
         </>
     );
 }
