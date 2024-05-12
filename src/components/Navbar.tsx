52259edaf7609294beddca982bc3b3c42b775837
Taesok Kwon <kwontaesok1997@gmail.com>
2024-05-12 02:53:29 -0700

    Update shared components UI (Footer, Home, Breadcrumb, etc)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import styled from '@emotion/styled';
-import myImage from '../assets/react.svg';
+import myImage from '../assets/amiibo.png';
+import { FaUser } from 'react-icons/fa';
 
 import { useAppSelector } from '../redux/hooks';
 import grabUserNameCapitalized from '../functions/grabUserName';
 
 const NavbarContainer = styled.header`
-    max-width: 100%;
     width: 100%;
-    margin: auto;
+    margin: 0 auto;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 1rem 1.5rem;
+    padding: 0.25rem 1.5rem;
     @media (min-width: 1024px) {
         max-width: 1280px;
     }
@@ -28,8 +28,7 @@ const NavbarLinkContainer = styled.div`
 
 const NavbarLogo = styled.a`
     flex: 1;
-    padding: 1.5rem;
-    margin: -0.375rem;
+    padding: 1rem;
     img {
         height: 2rem;
         width: auto;
@@ -37,7 +36,7 @@ const NavbarLogo = styled.a`
 `;
 
 const NavbarLink = styled(NavLink)`
-    font-size: 0.875rem;
+    font-size: 1rem;
     font-weight: 600;
     line-height: 1.5;
     color: #1f2937;
@@ -65,12 +64,12 @@ function Navbar() {
 
     return (
         <NavbarContainer>
-            <NavbarLogo href="#">
+            <NavbarLogo href="/">
                 <img src={myImage} alt="Amiibo Atlas" />
             </NavbarLogo>
             <NavbarLinkContainer>
                 <NavbarLink to="/home">Amiibo Atlas</NavbarLink>
-                <NavbarLink to="/amiibos">Amiibos Page</NavbarLink>
+                <NavbarLink to="/amiibos">Amiibos</NavbarLink>
                 <NavbarLink to="/wishlist">WishList</NavbarLink>
                 <NavbarLink to="/profile">Profile Page</NavbarLink>
             </NavbarLinkContainer>
@@ -78,7 +77,9 @@ function Navbar() {
                 {user.loginStatus ? (
                     <h2>Hello, {userNameCapitalized}</h2>
                 ) : (
-                    <NavbarLink to="/login">Log in →</NavbarLink>
+                    <NavbarLink to="/login">
+                        <FaUser />
+                    </NavbarLink>
                 )}
             </NavbarUserSection>
         </NavbarContainer>
