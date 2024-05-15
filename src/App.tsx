03322917f959d39e2750bc47b66895fd4943df15
Ari Zeto <arizetocs@gmail.com>
2024-05-15 12:42:31 -0700

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const CenterContent = styled.div`
 
 const SplashInfo = styled.div`
     padding: 2rem;
-    border: 2px solid black;
+    border: 1px solid gray;
     border-radius: 7px;
     background-color: white;
     transition: 0.3s ease;
@@ -76,10 +76,6 @@ export default function App() {
     const [toggle, setToggle] = useState(false);
     const { isLoading, error } = GetAmiibo();
 
-    // Grab user from user global state.
-    // const user = useAppSelector((state) => state.setUser);
-    // const userNameCapitalized = grabUserNameCapitalized(user);
-
     //
     const { userId } = useParams();
     const [user, setUser] = useState<User | null>(null);
@@ -102,14 +98,11 @@ export default function App() {
 
     // Selecting amiibo data from the redux store...
     const amiiboDataRedux = useAppSelector((state) => state.allAmiiboSlice.amiibos);
-    // console.log('Testing...: ', amiiboDataRedux);
 
     // Filter amiibo data based on allAmiibo state
     const filterAmiiboAllOrRecent = allAmiibo
         ? amiiboDataRedux
         : filterRecentReleases(amiiboDataRedux);
-
-    // console.log('Recent...: ', filterAmiiboAllOrRecent);
 
     if (isLoading) return <p>Loading...</p>;
     if (error) return <p>{error.message}</p>;
@@ -117,10 +110,10 @@ export default function App() {
     return (
         <>
             <Wrapper>
-                {/* <img src={myImage} /> */}
                 <h1>Welcome to Amiibo Atlas!</h1>
                 <SplashInfo>
                     <CenterContent>
+                        <h2>Featured Amiibo</h2>
                         <Scroll />
                     </CenterContent>
 
@@ -131,7 +124,7 @@ export default function App() {
                         ten years now, and have varied use cases amongst Nintendo’s catalog of
                         games.
                     </p>
-                    <br />
+
                     <p>
                         Core functionality includes authentication, parameterized pages, and various
                         the ability to wishlist particular amiibos, etc. The frontend was built
